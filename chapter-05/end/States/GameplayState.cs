72da7d2c4c26ac1061f407277c1e68a6f8fdbe1b
Jarred Capellman <jcapellman@hotmail.com>
2019-12-30 19:38:56 -0600

    Added: End Chapter 5 solution and assets

---
+++ chapter-05/end/States/GameplayState.cs
@@ -0,0 +1,31 @@
+﻿using chapter_05.Enum;
+using chapter_05.Objects;
+using chapter_05.States.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace chapter_05.States
+{
+    public class GameplayState : BaseGameState
+    {
+        private const string PlayerFighter = "F18";
+
+        private const string BackgroundTexture = "Barren";
+
+        public override void LoadContent()
+        {
+            AddGameObject(new SplashImage(LoadTexture(BackgroundTexture)));
+            AddGameObject(new PlayerSprite(LoadTexture(PlayerFighter)));
+        }
+
+        public override void HandleInput()
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                NotifyEvent(Events.GAME_QUIT);
+            }
+        }
+    }
+}
---
+++ chapter-05/end/States/SplashState.cs
@@ -0,0 +1,25 @@
+﻿using chapter_05.Objects;
+using chapter_05.States.Base;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace chapter_05.States
+{
+    public class SplashState : BaseGameState
+    {
+        public override void LoadContent()
+        {
+            AddGameObject(new SplashImage(LoadTexture("splash")));
+        }
+
+        public override void HandleInput()
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Enter))
+            {
+                SwitchState(new GameplayState());
+            }
+        }
+    }
+}

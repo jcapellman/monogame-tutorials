b3b273707a5d0f48802df98fe9889701c05647d2
Louis Salin <louis.phil@gmail.com>
2020-06-28 16:18:51 -0500

    Fix typo and uncomment DevState code
    This is the final (for real) update to this chapter's end code

--- chapter-08/end/States/Dev/DevState.cs
+++ chapter-08/end/States/Dev/DevState.cs
@@ -17,19 +17,17 @@ namespace chapter_08.States
         private const string PlayerFighter = "fighter";
 
         private ExhaustEmitter _exhaustEmitter;
-        private MissileSprite _missle;
+        private MissileSprite _missile;
         private PlayerSprite _player;
 
         public override void LoadContent()
         {
             var exhaustPosition = new Vector2(_viewportWidth / 2, _viewportHeight / 2);
-            //_exhaustEmitter = new ExhaustEmitter(LoadTexture(ExhaustTexture), exhaustPosition);
-            //AddGameObject(_exhaustEmitter);
-
-            _missle = new MissileSprite(LoadTexture(MissileTexture), LoadTexture(ExhaustTexture));
+            _exhaustEmitter = new ExhaustEmitter(LoadTexture(ExhaustTexture), exhaustPosition);
+            AddGameObject(_exhaustEmitter);
 
             _player = new PlayerSprite(LoadTexture(PlayerFighter));
-            _player.Position= new Vector2(500, 500);
+            _player.Position = new Vector2(500, 500);
             AddGameObject(_player);
         }
 
@@ -44,28 +42,31 @@ namespace chapter_08.States
 
                 if (cmd is DevInputCommand.DevShoot)
                 {
-                    _missle.Position = new Vector2(_player.Position.X, _player.Position.Y - 25);
-                    AddGameObject(_missle);
+                    _missile = new MissileSprite(LoadTexture(MissileTexture), LoadTexture(ExhaustTexture));
+                    _missile.Position = new Vector2(_player.Position.X, _player.Position.Y - 25);
+                    AddGameObject(_missile);
                 }
-
             });
         }
 
         public override void UpdateGameState(GameTime gameTime) 
         {
-            //_exhaustEmitter.Position = new Vector2(_exhaustEmitter.Position.X, _exhaustEmitter.Position.Y - 3f);
-            //_exhaustEmitter.Update(gameTime);
+            _exhaustEmitter.Position = new Vector2(_exhaustEmitter.Position.X, _exhaustEmitter.Position.Y - 3f);
+            _exhaustEmitter.Update(gameTime);
 
-            _missle.Update(gameTime);
-
-            //if (_exhaustEmitter.Position.Y < -200)
-            //{
-            //    RemoveGameObject(_exhaustEmitter);
-            //}
-
-            if (_missle.Position.Y < -100)
+            if (_missile != null)
             {
-                RemoveGameObject(_missle);
+                _missile.Update(gameTime);
+
+                if (_missile.Position.Y < -100)
+                {
+                    RemoveGameObject(_missile);
+                }
+            }
+ 
+            if (_exhaustEmitter.Position.Y < -200)
+            {
+                RemoveGameObject(_exhaustEmitter);
             }
         }
 

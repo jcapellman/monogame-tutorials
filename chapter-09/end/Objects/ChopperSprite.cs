6d6d0bd97fcd35398e16366a5f37ba400be258a0
Louis Salin <louis.phil@gmail.com>
2020-07-04 14:10:20 -0500

    add chapter 9 code

---
+++ chapter-09/end/Objects/ChopperSprite.cs
@@ -0,0 +1,16 @@
+﻿using chapter_09.Engine.Objects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace chapter_09.Objects
+{
+    public class ChopperSprite : BaseGameObject
+    {
+        private const float SPEED = 8.0f;
+
+        public ChopperSprite(Texture2D texture)
+        {
+            _texture = texture;
+        }
+    }
+}
---
+++ chapter-09/end/States/Dev/DevState.cs
@@ -0,0 +1,47 @@
+﻿using chapter_09.Engine.Input;
+using chapter_09.Engine.States;
+using chapter_09.Input;
+using chapter_09.Objects;
+using chapter_09.States.Particles;
+using Microsoft.Xna.Framework;
+
+namespace chapter_09.States
+{
+    /// <summary>
+    /// Used to test out new things, like particle engines and shooting missiles
+    /// </summary>
+    public class DevState : BaseGameState
+    {
+        private const string ExhaustTexture = "Cloud";
+        private const string ChopperTexture = "Chopper";
+
+        private ChopperSprite _chopper;
+
+        public override void LoadContent()
+        {
+            _chopper = new ChopperSprite(LoadTexture(ChopperTexture));
+            _chopper.Position = new Vector2(500, 500);
+            AddGameObject(_chopper);
+        }
+
+        public override void HandleInput(GameTime gameTime)
+        {
+            InputManager.GetCommands(cmd =>
+            {
+                if (cmd is DevInputCommand.DevQuit)
+                {
+                    NotifyEvent(new BaseGameStateEvent.GameQuit());
+                }
+            });
+        }
+
+        public override void UpdateGameState(GameTime gameTime) 
+        {
+        }
+
+        protected override void SetInputManager()
+        {
+            InputManager = new InputManager(new DevInputMapper());
+        }
+    }
+}

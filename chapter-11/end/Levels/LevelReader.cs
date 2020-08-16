8e2005f5b8ee526ae8de0f51049bc94ec04f1307
Louis Salin <louis.phil@gmail.com>
2020-08-16 18:36:42 -0500

    check for null when reading level

--- chapter-11/end/Levels/LevelReader.cs
+++ chapter-11/end/Levels/LevelReader.cs
@@ -33,11 +33,18 @@ namespace chapter_11.Levels
                 levelString = reader.ReadToEnd();
             }
 
-            var rows = levelString.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            var convertedRows = from r in rows
-                                select ToEventRow(r);
+            if (levelString != null && levelString.Length > 0)
+            {
+                var rows = levelString.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                var convertedRows = from r in rows
+                                    select ToEventRow(r);
 
-            return convertedRows.Reverse().ToList();
+                return convertedRows.Reverse().ToList();
+            }
+            else
+            {
+                return new List<List<BaseGameStateEvent>>();
+            }
         }
 
         private List<BaseGameStateEvent> ToEventRow(string rowString)
